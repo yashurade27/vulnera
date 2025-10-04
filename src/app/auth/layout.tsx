4fa40f07871da7bf34525152eb70246b255224a7
Prajyot Tayde <pmtbmt@gmail.com>
2025-10-04 13:56:12 +0530

    minor ui changes

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,9 +10,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Navbar />
-      <main className="flex-grow container mx-auto px-25 py-10 flex items-center justify-center">
-        {children}
-      </main>
+      <main className="flex-grow container mx-auto px-25 py-10 flex items-center justify-center">{children}</main>
       <Footer />
     </div>
   )
--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,26 +1,22 @@
 'use client'
 
 import React from 'react'
-import { ClusterUiChecker } from '@/features/cluster/ui/cluster-ui-checker'
-import { AccountUiChecker } from '@/features/account/ui/account-ui-checker'
+// import { ClusterUiChecker } from '@/features/cluster/ui/cluster-ui-checker'
+// import { AccountUiChecker } from '@/features/account/ui/account-ui-checker'
 import { ThemeProvider } from './theme-provider'
 import { Toaster } from './ui/sonner'
 import { AppHeader } from './app-header'
 import { AppFooter } from './app-footer'
 
-export function AppLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <div className="flex flex-col min-h-screen">
         <AppHeader />
-        <main className="flex-grow container mx-auto p-4">
-          <ClusterUiChecker>
+        <main className="flex-grow mt-16 container mx-auto p-4">
+          {/* <ClusterUiChecker>
             <AccountUiChecker />
-          </ClusterUiChecker>
+          </ClusterUiChecker> */}
           {children}
         </main>
         <AppFooter />
